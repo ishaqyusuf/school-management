253c5016d25aa0efc807351e135a24b5e9849922
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-18 10:58:09 +0100

    :tada:

--- app/[lng]/(auth)/session/[sessionSlug]/term/[termSlug]/students/page.tsx
+++ app/[lng]/(auth)/session/[sessionSlug]/term/[termSlug]/students/page.tsx
@@ -6,12 +6,6 @@ import SetStudentClassSheet from "@/components/sheets/set-student-class-sheet";
 import UpdateStudentPayableSheet from "@/components/sheets/update-student-payable";
 import Header from "@/components/header";
 export default async function StudentsPage({ searchParams, params }) {
-  await prisma.studentTermSheets.update({
-    where: { id: 31 },
-    data: {
-      owing: 1000,
-    },
-  });
   const students = await prisma.students.findMany({
     where: {},
     include: {
